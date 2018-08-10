16d90f47f495fe17cb7a95bb3a18fa4115571089
Neila Laviolette <neila.lav@gmail.com>
2018-08-10 18:49:27 -0500

    SearchView

--- app/src/main/java/com/codepath/nbpharmaapp/PharmaActivity.java
+++ app/src/main/java/com/codepath/nbpharmaapp/PharmaActivity.java
@@ -1,7 +1,16 @@
 package com.codepath.nbpharmaapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.support.v7.widget.SearchView;
+
+import de.greenrobot.event.EventBus;
+
+import static android.support.v4.view.MenuItemCompat.getActionView;
 
 public class PharmaActivity extends AppCompatActivity {
 
@@ -9,5 +18,32 @@ public class PharmaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pharma);
+
+        ListView lvMedoc=  findViewById(R.id.lvMedicament);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater= getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+       final MenuItem searchitem= menu.findItem(R.id.menu_Search);
+        SearchView searchView= (SearchView) getActionView(searchitem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                EventBus.getDefault().post(new QueryEvent(query));
+
+                invalidateOptionsMenu();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        return true;
     }
 }
---
+++ app/src/main/java/com/codepath/nbpharmaapp/QueryEvent.java
@@ -0,0 +1,13 @@
+package com.codepath.nbpharmaapp;
+
+public class QueryEvent {
+    String query;
+
+    public QueryEvent(String query){
+        this.query=query;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+}
